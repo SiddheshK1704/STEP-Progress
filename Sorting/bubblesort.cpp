c766f360a465aafe5764a1ef7246d73dd7600f6b
SiddheshK1704 <siddheshkhankhoje@gmail.com>
2026-06-22 14:51:29 +0530

    Imp comment added

--- Sorting/bubblesort.cpp
+++ Sorting/bubblesort.cpp
@@ -11,7 +11,7 @@ void BubbleSort(vector<int>& arr) {
     for (int i = 0; i < n - 1; i++) {
         swapped = false;
 
-        for (int j = 0; j < n - i - 1; j++) {
+        for (int j = 0; j < n - i - 1; j++) {//Worst case time complexity = O(n^2)
             if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
                 swapped = true;
